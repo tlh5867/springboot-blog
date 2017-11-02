1d75048c2e3747c9bf7659b31862bed1cd95a700
Tomas Hipolito <tlhipolito67@gmail.com>
2017-11-02 13:50:04 -0500

    -made corrections on the postController class

--- src/main/java/com/codeup/blog/controllers/PostController.java
+++ src/main/java/com/codeup/blog/controllers/PostController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-//must add controller always DON'T FORGET TOMAS!!!!!
+//must add controller always DON'T FORGET LEON!!!!!
 @Controller
 public class PostController {
 
@@ -17,19 +17,19 @@ public class PostController {
 
     @ResponseBody
     @GetMapping("/posts{id}")
-    public String indPost(){
-        return "<h1>View an individual post</h1>";
+    public String showPost(@PathVariable int id){
+        return "<h1>View an individual post</h1>"+ id;
     }
 
     @ResponseBody
     @GetMapping("/posts/create")
-    public String createPost(){
+    public String showCreateForm(){
         return "<h1>View the form for creating posts</h1>";
     }
 
     @ResponseBody
    @PostMapping("/posts/create")
-    public String post(){
+    public String createPost(){
         return "Create new post";
     }
 
