5328f5df14fea2c74d6841f87e3a254005a5b665
Tomas Hipolito <tlhipolito67@gmail.com>
2017-11-02 11:22:13 -0500

    -completed the math controller class exercise

--- src/main/java/com/codeup/blog/controllers/MathController.java
+++ src/main/java/com/codeup/blog/controllers/MathController.java
@@ -3,17 +3,34 @@ package com.codeup.blog.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MathController {
+    @ResponseBody
+    @GetMapping("/add/{numberOne}/and/{numberTwo}")
+    public Integer add(@PathVariable Integer numberOne,@PathVariable Integer numberTwo){
+        return numberOne + numberTwo;
+    }
+    @ResponseBody
+    @GetMapping("/subtract/{numberOne}/from/{numberTwo}")
+    public Integer sub(@PathVariable Integer numberOne,@PathVariable Integer numberTwo){
+        return numberOne - numberTwo;
 
-    @GetMapping("/add")
-    public Integer add(@PathVariable Integer number){
-        return number + number;
+    }
+    @ResponseBody
+    @GetMapping("/multiply/{numberOne}/and/{numberTwo}")
+    public Integer times(@PathVariable Integer numberOne,@PathVariable Integer numberTwo){
+        return numberOne * numberTwo;
+    }
+    @ResponseBody
+    @GetMapping("/divide/{numberOne}/by/{numberTwo}")
+    public Integer divide(@PathVariable Integer numberOne,@PathVariable Integer numberTwo){
+        return numberOne / numberTwo;
     }
 
-    @GetMapping("subtract")
-    public Integer sub(@PathVariable Integer number){
-        return number - number;
-    }
+
+
+
+
 }
